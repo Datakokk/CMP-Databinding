07c68f9e0796a46794199ebd6155f6952edca490
Datakokk <fiodorelzarster@gmx.com>
2021-03-03 13:35:38 +0100

    Lifecicle hooks in action

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,17 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { 
+    Component, 
+    Input, 
+    OnInit, 
+    ViewEncapsulation, 
+    OnChanges,
+    SimpleChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+  } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,12 +19,50 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'], 
   encapsulation: ViewEncapsulation.Emulated//ShadowDom, None
 })
-export class ServerElementComponent implements OnInit {
-  @Input('aliasElement') element: {type:string, name:string, content:string}
+export class ServerElementComponent implements 
+  OnInit, 
+  OnChanges, 
+  DoCheck, 
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
+  @Input('aliasElement') element22: {type:string, name:string, content:string}
+  @Input() name:string;
 
-  constructor() { }
+  constructor() { console.log("constructor called");}
 
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges){
+    console.log("ngOnChanges is  called!!!!");
+    console.log(changes);
   }
 
+  ngOnInit(): void {
+    console.log("ngOnInit called");
+  }
+
+  ngDoCheck(){
+    console.log('ngDoCheck called!!!!');
+  }
+
+  ngAfterContentInit(){
+    console.log("ngAfterContentInit is here!!!!");
+  }
+
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked is runingggggg!');
+  }
+
+  ngAfterViewInit(){
+    console.log("ngAfterViewInit is here!///////////!!!");
+  }
+
+  ngAfterViewChecked(){
+    console.log('ngAfterViewChecked is runing************!');
+  }
+
+  ngOnDestroy(){
+    console.log('ngOnDestroy calleddd the end!!!!!!!!!!!');
+  }
 }
